d1ed0efe0ed5d91ce1ab09a88b1f9a249244a7c4
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-29 07:57:58 -0300

    melhoando condicionais

--- index.js
+++ index.js
@@ -1,56 +1,74 @@
+// Deve imprimir um dos seguintes textos:
+
+//       Frutas: abacaxi
+//       Frutas: banana
+//       Frutas: uva
+
+//       Frutas: abacaxi e Banana
+//       Frutas:  banana e uva
+//       Frutas:  abacaxi, banana e uva
+
+//       Legumes: abobrinha
+//       Legumes: cenoura
+//       Legumes: abobrinha e cenoura
+
+//       Resumo: Comprei somente frutas
+//       Resumo: Comprei somente legumes
+//       Resumo: Comprei frutas e legumes
+
+//       Resumo: Não comprei nada
+
+// &&(E) --   || (Ou)
 
 
-const compreAbacaxi = true;
-const compreiBanana = true;
-const compreiUva = true;
 
-const compreiAbobrinha = true;
-const compreiCenoura = true;
+const compreiAbacaxi = false;
+const compreiBanana = false;
+const compreiUva = false;
 
-
-
-const compreiAbacaxiEBanana = compreAbacaxi && compreiBanana;
-const compreiBananaEUva = compreiBanana && compreiUva;
-const compreiAbacaxiEUva = compreAbacaxi && compreiUva;
+const compreiBananaEUva = compreiBanana && compreiUva && !compreiAbacaxi;
+const compreiAbacaxiEBanana = compreiAbacaxi && compreiBanana;
+const compreiTodasAsFrutas = compreiAbacaxi && compreiBanana && compreiUva;
 
 
-const compreiTodasAsFrutas = compreAbacaxi && compreiBanana && compreiUva;
+const compreiAbobrinha = false;
+const compreiCenoura = false;
 
 const compreiTodosOsLegumes = compreiAbobrinha && compreiCenoura;
 
 
-const compreiSomenteFrutas = compreiTodasAsFrutas && !compreiTodosOsLegumes;
-const compreiSomenteLegumes = compreiTodosOsLegumes && !compreiTodasAsFrutas;
-const compreiFrutasELegumes = compreiTodasAsFrutas && compreiTodosOsLegumes;
-
-
-
-if (compreiFrutasELegumes) {
-    console.log("Resumo: Comprei frutas e legumes");
-} else if (compreiSomenteFrutas) {
-    console.log("Resumo: Comprei somente frutas");
-} else if (compreiSomenteLegumes) {
-    console.log("Resumo: Comprei somente legumes");
-} else if (compreiTodasAsFrutas) {
-    console.log("Frutas: abacaxi, banana e uva");
-} else if (compreiTodosOsLegumes) {
-    console.log("Legumes: abobrinha e cenoura");
-} else if (compreiAbacaxiEBanana) {
-    console.log("Frutas: abacaxi e Banana");
-} else if (compreiBananaEUva) {
-    console.log("Frutas: banana e uva");
-} else if (compreiAbacaxiEUva) {
-    console.log("Frutas: abacaxi e uva");
-} else if (compreAbacaxi) {
-    console.log("Frutas: abacaxi");
-} else if (compreiBanana) {
-    console.log("Frutas: banana");
-} else if (compreiUva) {
+// Frutas:
+if(compreiTodasAsFrutas){
+    console.log("Frutas:   abacaxi, banana e uva");
+} else if(compreiBananaEUva){
+    console.log("Frutas:   banana e uva");
+} else if(compreiAbacaxiEBanana){
+    console.log("Frutas: abacaxi e banana");
+}else if(compreiAbacaxi){
+    // Corrigido: 'comnsole' para 'console'
+    console.log("Fruta: abacaxi");
+} else if(compreiBanana){
+    console.log("Frutas: banana ");
+} else if(compreiUva){
     console.log("Frutas: uva");
-} else if (compreiAbobrinha) {
-    console.log("Legumes: abobrinha");
-} else if (compreiCenoura) {
+}
+
+// Legumes:
+if(compreiTodosOsLegumes){
+    console.log("Legumes: abobrinha e cenoura");
+} else if(compreiAbobrinha){
+    console.log("Legumes: abobrinhas");
+} else if(compreiCenoura){
     console.log("Legumes: cenoura");
+}
+
+// Resumo: Variáveis corrigidas para 'comprei'
+if ((compreiAbacaxi || compreiBanana || compreiUva) && !(compreiAbobrinha || compreiCenoura)) {
+  console.log("Resumo: Comprei somente frutas");
+} else if (!(compreiAbacaxi || compreiBanana || compreiUva) && (compreiAbobrinha || compreiCenoura)) {
+  console.log("Resumo: Comprei somente legumes");
+} else if ((compreiAbacaxi || compreiBanana || compreiUva) && (compreiAbobrinha || compreiCenoura)) {
+  console.log("Resumo: Comprei frutas e legumes");
 } else {
-    console.log(" Não comprei nada");
+  console.log("Resumo: Não comprei nada");
 }
